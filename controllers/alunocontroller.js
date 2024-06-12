cd70a383e0c4fae114e017851157df05c3e126a4
Kipperfeito <heitor.k.w@gmail.com>
2024-06-12 08:21:32 -0300

    controlador e rotas

--- controllers/alunocontroller.js
+++ controllers/alunocontroller.js
@@ -1,19 +1,37 @@
 import Usuario from '../models/Usuario.js'
+import Aluno from '../models/Aluno.js'
 
 export function abreCadastro(req, res) {
     res.render("cadastro.ejs")
 }
 export async function cadastro(req, res) {
     var mat = Math.floor(Math.random() * 100000000).toString()
-    console.log(mat)
     const usuario = new Usuario({
         nome: req.body.nome,
         email: req.body.email,
         matricula: "PR" + "-" + mat,
         foto: req.file.filename,
         senha: req.body.senha,
-        admin: req.body.admin=="admin"?true:false
+        admin: req.body.admin == "admin" ? true : false
     })
     await usuario.save()
     console.log(usuario.matricula)
 }
+
+export function abreCadastroAl(req, res) {
+    res.render("cadastroAluno.ejs")
+}
+
+export async function cadastroAl(req, res) {
+    var mat = Math.floor(Math.random() * 100000000).toString()
+    const aluno = new Aluno({
+        nome: req.body.nome,
+        email: req.body.email,
+        matricula: "AL" + "-" + mat,
+        foto: req.file.filename,
+    })
+    await aluno.save()
+    console.log(aluno.matricula)
+}
+
+
--- routes/escola.js
+++ routes/escola.js
@@ -2,12 +2,16 @@ import express from 'express';
 const router = express.Router();
 import multer from 'multer';
 const foto = multer({
-    dest:'./public/foto'
+    dest: './public/foto'
 })
 
 
-import { cadastro, abreCadastro } from '../controllers/alunocontroller.js';
+import { cadastro, abreCadastro, abreCadastroAl, cadastroAl } from '../controllers/alunocontroller.js';
 
 router.get('/cadastro', abreCadastro)
-router.post('/cadastro',foto.single('foto'), cadastro)
+router.post('/cadastro', foto.single('foto'), cadastro)
+
+
+router.post('/cadastroAl', foto.single('foto'), cadastroAl)
+
 export default router;
